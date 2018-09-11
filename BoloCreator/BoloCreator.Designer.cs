39df38e13399882e7ae199471281ac1c21280318
OlympianCode <johnmcmahon427@gmail.com>
2018-09-11 00:57:42 -0700

    fixed bugs including not picking up all images due to lack of looking at file extensions correctly, fixed bug that prevented explorer from opening due to events being tied to links before links were fully created by moving event assignment to documentcompleted event. Also slight redesign to form, css, and significant refactoring of code to begin including settings options

--- BoloCreator/BoloCreator.Designer.cs
+++ BoloCreator/BoloCreator.Designer.cs
@@ -28,91 +28,112 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.btnChoose = new System.Windows.Forms.Button();
-            this.txtPath = new System.Windows.Forms.TextBox();
-            this.grpBrowser = new System.Windows.Forms.GroupBox();
             this.webBrowser1 = new System.Windows.Forms.WebBrowser();
             this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
-            this.btnHelp = new System.Windows.Forms.Button();
-            this.grpBrowser.SuspendLayout();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.openFolderToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.editToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.helpToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.settingsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.clearToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
             this.SuspendLayout();
-            // 
-            // btnChoose
-            // 
-            this.btnChoose.Location = new System.Drawing.Point(12, 10);
-            this.btnChoose.Name = "btnChoose";
-            this.btnChoose.Size = new System.Drawing.Size(141, 23);
-            this.btnChoose.TabIndex = 0;
-            this.btnChoose.Text = "Choose Root Folder...";
-            this.btnChoose.UseVisualStyleBackColor = true;
-            this.btnChoose.Click += new System.EventHandler(this.btnChoose_Click);
-            // 
-            // txtPath
-            // 
-            this.txtPath.Location = new System.Drawing.Point(159, 12);
-            this.txtPath.Name = "txtPath";
-            this.txtPath.Size = new System.Drawing.Size(1100, 20);
-            this.txtPath.TabIndex = 1;
-            // 
-            // grpBrowser
-            // 
-            this.grpBrowser.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
-            | System.Windows.Forms.AnchorStyles.Left) 
-            | System.Windows.Forms.AnchorStyles.Right)));
-            this.grpBrowser.AutoSize = true;
-            this.grpBrowser.Controls.Add(this.webBrowser1);
-            this.grpBrowser.Location = new System.Drawing.Point(12, 68);
-            this.grpBrowser.Name = "grpBrowser";
-            this.grpBrowser.Size = new System.Drawing.Size(1253, 700);
-            this.grpBrowser.TabIndex = 2;
-            this.grpBrowser.TabStop = false;
             // 
             // webBrowser1
             // 
             this.webBrowser1.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.webBrowser1.Location = new System.Drawing.Point(3, 16);
+            this.webBrowser1.Location = new System.Drawing.Point(0, 24);
             this.webBrowser1.MinimumSize = new System.Drawing.Size(20, 20);
             this.webBrowser1.Name = "webBrowser1";
-            this.webBrowser1.Size = new System.Drawing.Size(1247, 681);
+            this.webBrowser1.Size = new System.Drawing.Size(1044, 717);
             this.webBrowser1.TabIndex = 0;
             this.webBrowser1.DocumentCompleted += new System.Windows.Forms.WebBrowserDocumentCompletedEventHandler(this.webBrowser1_DocumentCompleted);
             this.webBrowser1.Navigating += new System.Windows.Forms.WebBrowserNavigatingEventHandler(this.webBrowser1_Navigating);
             // 
-            // btnHelp
+            // menuStrip1
             // 
-            this.btnHelp.Location = new System.Drawing.Point(12, 39);
-            this.btnHelp.Name = "btnHelp";
-            this.btnHelp.Size = new System.Drawing.Size(65, 23);
-            this.btnHelp.TabIndex = 3;
-            this.btnHelp.Text = "Help!";
-            this.btnHelp.UseVisualStyleBackColor = true;
-            this.btnHelp.Click += new System.EventHandler(this.btnHelp_Click);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem,
+            this.editToolStripMenuItem,
+            this.helpToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(1044, 24);
+            this.menuStrip1.TabIndex = 4;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.openFolderToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // openFolderToolStripMenuItem
+            // 
+            this.openFolderToolStripMenuItem.Name = "openFolderToolStripMenuItem";
+            this.openFolderToolStripMenuItem.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.O)));
+            this.openFolderToolStripMenuItem.Size = new System.Drawing.Size(191, 22);
+            this.openFolderToolStripMenuItem.Text = "Open Folder...";
+            this.openFolderToolStripMenuItem.Click += new System.EventHandler(this.openFolderToolStripMenuItem_Click);
+            // 
+            // editToolStripMenuItem
+            // 
+            this.editToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.settingsToolStripMenuItem,
+            this.clearToolStripMenuItem});
+            this.editToolStripMenuItem.Name = "editToolStripMenuItem";
+            this.editToolStripMenuItem.Size = new System.Drawing.Size(39, 20);
+            this.editToolStripMenuItem.Text = "Edit";
+            // 
+            // helpToolStripMenuItem
+            // 
+            this.helpToolStripMenuItem.Name = "helpToolStripMenuItem";
+            this.helpToolStripMenuItem.Size = new System.Drawing.Size(44, 20);
+            this.helpToolStripMenuItem.Text = "Help";
+            this.helpToolStripMenuItem.Click += new System.EventHandler(this.helpToolStripMenuItem_Click);
+            // 
+            // settingsToolStripMenuItem
+            // 
+            this.settingsToolStripMenuItem.Name = "settingsToolStripMenuItem";
+            this.settingsToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.settingsToolStripMenuItem.Text = "Settings";
+            // 
+            // clearToolStripMenuItem
+            // 
+            this.clearToolStripMenuItem.Name = "clearToolStripMenuItem";
+            this.clearToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.clearToolStripMenuItem.Text = "Clear";
             // 
             // BoloCreator
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1271, 780);
-            this.Controls.Add(this.btnHelp);
-            this.Controls.Add(this.grpBrowser);
-            this.Controls.Add(this.txtPath);
-            this.Controls.Add(this.btnChoose);
+            this.ClientSize = new System.Drawing.Size(1044, 741);
+            this.Controls.Add(this.webBrowser1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
             this.Name = "BoloCreator";
             this.Text = "BoloCreator";
             this.Load += new System.EventHandler(this.BoloCreator_Load);
-            this.grpBrowser.ResumeLayout(false);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
         }
 
         #endregion
-
-        private System.Windows.Forms.Button btnChoose;
-        private System.Windows.Forms.TextBox txtPath;
-        private System.Windows.Forms.GroupBox grpBrowser;
         private System.Windows.Forms.WebBrowser webBrowser1;
         private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
-        private System.Windows.Forms.Button btnHelp;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem openFolderToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem editToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem helpToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem settingsToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem clearToolStripMenuItem;
     }
 }
---
+++ BoloCreator/Settings.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BoloCreator
+{
+    class Settings
+    {
+        public bool SortNewestFirst { get; set; } = true;
+        public bool PutApprehensionPhotosFirst { get; set; } = true;
+        public string ApprehensionFolderName{ get; set; } = "APP";
+        public string PmrFolderName { get; set; } = "PMR";
+        public string KtrFolderName { get; set; } = "KTR";
+        public bool IncludeApprehensions { get; set; } = true;
+        public bool IncludePmrs { get; set; } = true;
+        public bool IncludeKtrs { get; set; } = true;
+        public bool IncludeMisc { get; set; } = true;
+        public bool SortByType { get; set; } = true;
+
+
+
+        public Settings() { }
+    }
+
+}
